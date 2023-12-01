85d3625a46b156d5f267dba9a43256e65ed5010b
jesusra11 <130915166+jesusra11@users.noreply.github.com>
2023-12-01 12:30:51 -0400

    Update hack_3.py

--- hack_3.py
+++ hack_3.py
@@ -15,7 +15,16 @@ text: "qux" output => "QvX"
 """
 
 
-def fn_hack_3():
-    result = "fooziman"
-    #...
+def fn_hack_3(result):
+
+        
+    list_comp = [("a","@"),("e","3"),("i","¡"),("o","0"),("u","v"),("A","@"),("E","3"),("I","¡"),("O","0"),("U","v")]
+
+    if len(result)>=2:      
+        result = result[0].upper() + result[1:-1] + result[-1].upper()
+
+    for i in result:
+        for x in list_comp:
+            if i in x[0]: result = result.replace(i,x[1])
+    
     return result
